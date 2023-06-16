70cf402dee07a5c39fd9bac65735d4ffc7fe03ea
arifrahmadi23 <muhammad231arif@gmail.com>
2023-06-16 11:11:51 +0700

    char ch;  while (true) {   cout << endl;   cout << "\n***Stack Menu***\n";   cout << "1. Push\n";   cout << "2. Pop\n";   cout << "3. Display\n";   cout << "4. Exit\n";   cout << "\nEnter an Element: ";   cin >> ch;   switch (ch) {

--- stackinarray/stackinarray.cpp
+++ stackinarray/stackinarray.cpp
@@ -59,7 +59,17 @@ int main() {
 	StackArray s;
 	char ch;
 	while (true) {
-		cout <<
+		cout << endl;
+		cout << "\n***Stack Menu***\n";
+		cout << "1. Push\n";
+		cout << "2. Pop\n";
+		cout << "3. Display\n";
+		cout << "4. Exit\n";
+		cout << "\nEnter an Element: ";
+		cin >> ch;
+		switch (ch) {
+
+		}
 	}
 	
 }
