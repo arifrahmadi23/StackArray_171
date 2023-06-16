661be744bde43be56dd870887a22adf4f2dd28c0
arifrahmadi23 <muhammad231arif@gmail.com>
2023-06-16 11:17:16 +0700

    case '1': {    cout << "\nElement an Element: ";    int element;    cin >> element;    s.push(element);    break;   }   case '2':    if (s.empty()) {     cout << "\nStack is empty." << endl;     break;    }    s.pop();    break;   case '3':    s.display();    break;   case '4':    return 0;   default:    cout << "\nInvalis choice" << endl;    break;   }  }
    }

--- stackinarray/stackinarray.cpp
+++ stackinarray/stackinarray.cpp
@@ -75,6 +75,21 @@ int main() {
 			s.push(element);
 			break;
 		}
+		case '2':
+			if (s.empty()) {
+				cout << "\nStack is empty." << endl;
+				break;
+			}
+			s.pop();
+			break;
+		case '3':
+			s.display();
+			break;
+		case '4':
+			return 0;
+		default:
+			cout << "\nInvalis choice" << endl;
+			break;
 		}
 	}
 	
