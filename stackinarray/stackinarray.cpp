8e12a0007cf1c16ca8128230500f19d47fa5b476
arifrahmadi23 <muhammad231arif@gmail.com>
2023-06-16 11:19:51 +0700

    void display() {   if (empty()) {    cout << "\nStack is empty." << endl;   }   else {    for (int tmp = top; tmp >= 0; tmp--) {     cout << stack_array[tmp] << endl;    }   }  }
    (MEMPERBAIKI)

--- stackinarray/stackinarray.cpp
+++ stackinarray/stackinarray.cpp
@@ -48,7 +48,7 @@ public:
 			cout << "\nStack is empty." << endl;
 		}
 		else {
-			for (int tmp = top; tmp >= 0; tmp++) {
+			for (int tmp = top; tmp >= 0; tmp--) {
 				cout << stack_array[tmp] << endl;
 			}
 		}
